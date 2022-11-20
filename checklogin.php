47af49409bbf3a1260a0d3c98a1a5a8f0e143898
GUSSCH <steamjg04@gmail.com>
2022-11-20 19:28:19 +0100

    removed folder

---
+++ checklogin.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Välkommen!</h1>
+    <?php
+        session_start();
+        if (isset($_SESSION["username"]) && !empty($_SESSION["username"])) {
+            echo "Du är inloggad som " . $_SESSION["username"] . ".";
+        }
+        else {
+            echo "Du är inte inloggad.";
+        }    
+    ?>
+    <br><br>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="fileToUpload" id="fileToUpload" />
+        <br><br>
+        <input type="submit" value="Ladda upp" name="submit" />
+    </form>
+    <br><br><a href='logout.php'>Logga ut</a><br><br>
+</body>
+</html>
--- skript.php
+++ skript.php
@@ -11,7 +11,7 @@
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
-	$dbname = "uppgift-4";
+	$dbname = "users";
 	$conn = new mysqli($servername, $username, $password, $dbname);
 
 	$sql = "SELECT * FROM users";
@@ -19,16 +19,22 @@
 
 	if ($result->num_rows > 0) {
 		while($row = $result->fetch_assoc()) {
-			if($row["username"] == $_POST["username"] && $row["password"] == $_POST["password"]){
+			if($row["userId"] == $_POST["username"] && $row["passwd"] == $_POST["password"]){
 				header("Location: checklogin.php");
 			}
 			else {
 				echo "<h1>Inloggningen misslyckades!</h1><p>Fel kombination av användarnamn och lösenord!</p>";	
-				echo "<br><br><a href='uppgift6.html'>Tillbaka</a>";
+				echo "<br><br><a href='uppgift7.html'>Tillbaka</a>";
 			}
 		}
 	} 
 	$conn->close();
+
+	if($login_success) {
+		session_start();
+		$_SESSION["username"] = $_POST["username"];
+	}
+	
 ?>
 </body>
 </html>
